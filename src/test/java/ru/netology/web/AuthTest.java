4f24d6cfee0f83d5f1bcdeedd592d7fd4cd48987
Aleksandr Nikitin <nan.alek@yandex.ru>
2021-05-26 21:30:29 +0300

    Com

--- src/test/java/ru/netology/web/AuthTest.java
+++ src/test/java/ru/netology/web/AuthTest.java
@@ -2,7 +2,10 @@ package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -10,48 +13,50 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.By.cssSelector;
 
 public class AuthTest {
-    @Test
-    void shouldLogInIfUserIsValid (){
-        Registration user = Generation.generateNewActiveValidUser();
+    @BeforeEach
+    public void setUp() {
         open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldLogInIfUserIsValid() {
+        Registration user = Generation.generateNewActiveValidUser();
+
         SelenideElement form = $("[action]");
         form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
         form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
         form.$(cssSelector("[type=button]")).click();
-        $(byText("Личный кабинет")).waitUntil(Condition.visible, 15000);
+        $(byText("Личный кабинет")).shouldBe(Condition.visible, Duration.ofSeconds(15000));
     }
 
     @Test
     void shouldNotLogInIfBlockedUser() {
         Registration user = Generation.generateNewBlockedUser();
-        open("http://localhost:9999");
+
         SelenideElement form = $("[action]");
         form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
         form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
         form.$(cssSelector("[type=button]")).click();
-        $(byText("Пользователь заблокирован")).waitUntil(Condition.visible, 15000);
+        $(byText("Пользователь заблокирован")).shouldBe(Condition.visible, Duration.ofSeconds(15000));
     }
 
     @Test
-
     void shouldNotLogInIfInvalidLogin() {
         Registration user = Generation.generateNewActiveUserInvalidLogin();
-        open("http://localhost:9999");
         SelenideElement form = $("[action]");
         form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
         form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
         form.$(cssSelector("[type=button]")).click();
-        $(byText("Неверно указан логин или пароль")).waitUntil(Condition.visible, 15000);
+        $(byText("Неверно указан логин или пароль")).shouldBe(Condition.visible, Duration.ofSeconds(15000));
     }
 
     @Test
     void shouldNotLogInIfInvalidPassword() {
         Registration user = Generation.generateNewActiveInvalidPassword();
-        open("http://localhost:9999");
         SelenideElement form = $("[action]");
         form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
         form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
         form.$(cssSelector("[type=button]")).click();
-        $(byText("Неверно указан логин или пароль")).waitUntil(Condition.visible, 15000);
+        $(byText("Неверно указан логин или пароль")).shouldBe(Condition.visible, Duration.ofSeconds(15000));
     }
 }
